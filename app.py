40c49de19ecdf402dbdf5fb2d2ad402d4cce4998
nemalanlb <nemalanbaskaran@gmail.com>
2026-08-04 22:41:55 +0530

    Changes

--- app.py
+++ app.py
@@ -50,12 +50,7 @@ webrtc_streamer(
         "iceServers": [
             {"urls": ["stun:stun.l.google.com:19302"]},
             {
-                "urls": ["turn:openrelay.metered.ca:80"],
-                "username": "openrelayproject",
-                "credential": "openrelayproject",
-            },
-            {
-                "urls": ["turn:openrelay.metered.ca:443"],
+                "urls": ["turn:openrelay.metered.ca:443?transport=tcp"],
                 "username": "openrelayproject",
                 "credential": "openrelayproject",
             },
