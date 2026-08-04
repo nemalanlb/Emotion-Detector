21d880cfc99645780b3eadb79954e6d76bc9534f
nemalanlb <nemalanbaskaran@gmail.com>
2026-08-04 22:36:21 +0530

    Changes

--- app.py
+++ app.py
@@ -46,5 +46,19 @@ st.title("Real-Time Facial Emotion Detection")
 webrtc_streamer(
     key="emotion-detect",
     video_processor_factory=EmotionProcessor,
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    rtc_configuration={
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]},
+            {
+                "urls": ["turn:openrelay.metered.ca:80"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject",
+            },
+            {
+                "urls": ["turn:openrelay.metered.ca:443"],
+                "username": "openrelayproject",
+                "credential": "openrelayproject",
+            },
+        ]
+    },
 )
